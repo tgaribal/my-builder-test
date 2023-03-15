a6fa8d0475a93140bdd17e97a1137bdb461d657e
Tim Garibaldi <tgaribal@gmail.com>
2023-03-14 22:50:10 -0700

    remove extra character

--- middleware.js
+++ middleware.js
@@ -10,14 +10,10 @@ export default function middleware(request) {
     if (request.nextUrl.pathname.startsWith('/query')) {
       const pathname = request.nextUrl.pathname;
       const searchParams = request.nextUrl.searchParams
-      console.log(searchParams)
       const something = encodeOptions({
         user: searchParams.get('user'),
         kw: searchParams.get('kw')
       })
-      console.log('HELLO SOMETHING: ', something)
-
-      console.log('HELLO REQUEST NEXTURL: ', `${pathname}/${something}`, request.url);
 
     return NextResponse.rewrite(new URL(`${pathname}/${something}`, request.nextUrl))
   }
